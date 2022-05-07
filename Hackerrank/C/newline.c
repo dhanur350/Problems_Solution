852eae5823fd84fb778f6129b532f5fa2b67563e
dhanur350 <dhanur297@gmail.com>
2022-05-07 12:52:31 +0530

    Day5.1

---
+++ Hackerrank/C/newline.c
@@ -0,0 +1,29 @@
+// Print sentence each word in new line
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    // Write your logic to print the tokens of the sentence here.
+    int len = strlen(s);
+    for (i = 0; i < len; i++)
+    {
+        if (s[i] == ' ')
+        {
+            printf("\n");
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    free(s);
+    return 0;
+}
